e005013cb631b693603c90400c3d0e5cc0392e48
Matthias Barkowsky <matthias.barkowsky@student.hpi.uni-potsdam.de>
2017-05-18 13:16:24 +0200

    fixed NullPointerException in prettyPrint method

--- MyUCCDetector/src/main/java/de/metanome/algorithms/myuccdetector/MyUCCDetectorAlgorithm.java
+++ MyUCCDetector/src/main/java/de/metanome/algorithms/myuccdetector/MyUCCDetectorAlgorithm.java
@@ -104,7 +104,12 @@ public class MyUCCDetectorAlgorithm {
         for (List<String> record: records) {
             i = 0;
             for (String value : record) {
-                maxCellLength[i] = Math.max(maxCellLength[i], value.length());
+            	if(value == null){
+            		maxCellLength[i] = Math.max(maxCellLength[i], "null".length());
+            	}
+            	else{
+            		maxCellLength[i] = Math.max(maxCellLength[i], value.length());
+            	}
                 i++;
             }
         }
@@ -127,7 +132,7 @@ public class MyUCCDetectorAlgorithm {
             System.out.print("| ");
             for (String value : record) {
                 System.out.print(value);
-                for (int k = 0; k < maxCellLength[i] - value.length(); k++)
+                for (int k = 0; k < (value != null ? maxCellLength[i] - value.length() : maxCellLength[i] - "null".length()) ; k++)
                     System.out.print(" ");
 
                 System.out.print(" | ");
